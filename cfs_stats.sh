04dca5996335c5aad17e644c3a07072642265c76
Benjamin Zofcin <zofcin@unlv.nevada.edu>
2021-03-09 23:00:26 -0800

    asdf

--- cfs_stats.sh
+++ cfs_stats.sh
@@ -1,1 +1,11 @@
-echo "Type in name of program to run"
+gcc -std=c99 -Wall -g -pthread -o leib leib_par.c
+./leib 1 &
+VAL=$(pgrep leib)
+# echo $VAL
+
+while :
+do
+cat /proc/$VAL/sched
+sleep 2
+done
+pkill leib
