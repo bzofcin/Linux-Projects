15b5ba61b275786b525be4a46658693bd015bc0e
bzofcin <31146185+bzofcin@users.noreply.github.com>
2022-10-19 11:42:25 -0400

    io & cpu bound
    Added files for io & cpu bound processes

--- cfs_stats.sh
+++ cfs_stats.sh
@@ -1,11 +1,21 @@
-gcc -std=c99 -Wall -g -pthread -o leib leib_par.c
+function handler(){
+    pkill leib
+    return -1
+}
+
+trap handler SIGINT
+
+gcc -std=c99 -o leib io_bound.c
+# gcc -std=c99 -o leib cpu_bound.c
+# gcc -std=c99 -g -pthread -o leib leib_par.c
 ./leib 1 &
 VAL=$(pgrep leib)
-# echo $VAL
+echo $VAL
 
-while :
+while [[ -f /proc/$VAL/sched ]]
 do
 cat /proc/$VAL/sched
 sleep 2
 done
-pkill leib
+
+rm leib
